847c3ff1b5bf534dc64a57ee0af28cc850687be1
Alex Wilson <fonzmp@gmail.com>
2018-05-21 16:11:33 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -3,13 +3,14 @@ class User
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
-
-  def first_name(name)
-    @first_name = name
   end
 
-  def last_name(name)
-    @last_name = name
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
   end
 
 end
